cc734330fbfef8064d40894c6219032a6c60e13a
MSUMichael <yr9608rk@go.minnstate.edu>
2024-11-06 13:43:39 -0600

    Michael's Audio Push
    3rd times the charm, surely it'll work now that I have made a completely new branch

---
+++ SingleStrike/Assets/Scripts/Michael/CombatMusicPlayer.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.Examples;
+using UnityEngine;
+
+public class CombatMusicPlayer : MonoBehaviour
+{
+    //public AudioClip passiveSound;
+    public AudioClip combatSound;
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        //audioSource.clip = passiveSound;
+        //audioSource.loop = true;
+        //audioSource.Play();
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    //Playing
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("EnemyHurtbox"))
+        {
+            audioSource.clip = combatSound;
+            audioSource.loop = true;
+            audioSource.Play();
+
+        }
+        else
+        {
+
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+
+    {
+
+        if (other.CompareTag("EnemyHurtbox"))
+        {
+
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("EnemyHurtbox"))
+        {
+            StartCoroutine(FadeAudioSource.StartFade(audioSource, 2.0f, 0));
+
+
+            //audioSource.clip = passiveSound;
+            //audioSource.loop = true;
+            //audioSource.Play();
+        }
+
+    }
+
+}
+
+
