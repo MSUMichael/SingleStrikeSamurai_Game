92143c7c9c9e7f094fe0dc8c5e1ffa856d545179
MSUMichael <yr9608rk@go.minnstate.edu>
2024-11-05 19:06:41 -0600

    Audio Update
    Background environmental noise now plays and fades out near enemies, likewise when near enemies combat music starts to play then fades when enemies die.
    
    Also I made it so that the "sword draw" sound effect is only 35% volume because my ears exploded every time I got close to an enemy.

---
+++ SingleStrike/Assets/Scripts/Michael/PassiveMusicPlayer.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.Examples;
+using UnityEngine;
+
+public class PassiveMusicPlayer : MonoBehaviour
+{
+    public AudioClip passiveSound;
+    //public AudioClip combatSound; 
+    public AudioSource audioSource;
+    
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        audioSource.clip = passiveSound;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    //Playing
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("EnemyHurtbox"))
+        {
+            StartCoroutine(FadeAudioSource.StartFade(audioSource, 2.0f, 0));
+
+        }
+        else
+        {
+
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+
+    {
+        
+        if (other.CompareTag("EnemyHurtbox"))
+        {
+
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("EnemyHurtbox"))
+        {
+            audioSource.clip = passiveSound;
+            audioSource.volume = 1.0f;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+
+    }
+
+}
+
+
